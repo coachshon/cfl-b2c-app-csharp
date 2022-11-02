e4206c221a6e34e9fcfb7a575d7b7d93df1be460
coachshon <shon.hogan@gmail.com>
2022-11-02 15:51:59 -0400

    modified azure deploy files

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -13,9 +13,11 @@ appName=""
 location=""
 
 # Connection string
-azureSQL='Server=tcp:.database.windows.net,1433;Initial Catalog=todo_v5;Persist Security Info=False;User ID=webapp;Password=Super_Str0ng*P4ZZword!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+#azureSQL='Server=tcp:.database.windows.net,1433;Initial Catalog=todo_v5;Persist Security Info=False;User ID=webapp;Password=Super_Str0ng*P4ZZword!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+azureSQL='Server=tcp:sql-cfl-api-dev.database.windows.net,1433;Initial Catalog=sqldb-cfl-api;Persist Security Info=False;User ID=hyonadmin;Password=9ZNkJe5BVZV;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
 
-gitSource="https://github.com/Azure-Samples/azure-sql-db-fullstack-serverless-kickstart"
+#gitSource="https://github.com/Azure-Samples/azure-sql-db-fullstack-serverless-kickstart"
+gitSource="https://github.com/coachshon/cfl-b2c-app-csharp"
 gitToken=""
 EOF
 	echo "Enviroment file not detected."
@@ -39,8 +41,8 @@ az deployment group create \
     location=$location \
     repositoryToken=$gitToken \
     repositoryUrl=$gitSource \
-    branch="v5.0" \
-    appLocation="./client" \
+    branch="v1.0" \
+    appLocation="./app/vue" \
     apiLocation="./api" \
     azureSQL="$azureSQL"
 
